bef8df628d1477c85cedac36257ae67804381daf
wwdo <onewoo90@gmail.com>
2026-03-05 15:53:15 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// 기존에 있던 import 문들은 그대로 둡니다.
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/CEO_KEYWORD_GAME/',   // 이 줄을 꼭 추가해 주세요!
+  // 기존에 있던 다른 설정들...
 })
